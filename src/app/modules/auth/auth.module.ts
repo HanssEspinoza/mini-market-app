5c52bd6fbf02dce26cd14d0b7bda0c87ed184181
ajed27 <98788271+ajed27@users.noreply.github.com>
2023-04-27 08:35:17 -0600

    feat: :construction: add register component

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
